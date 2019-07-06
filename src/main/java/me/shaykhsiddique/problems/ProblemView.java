02baaab858b767ce620c469d354ab7b690fd31da
shaykhsiddique <shaykhsiddique.lava@gmail.com>
2019-07-06 18:37:24 +0600

    add-problem page

---
+++ src/main/java/me/shaykhsiddique/problems/ProblemView.java
@@ -0,0 +1,84 @@
+package me.shaykhsiddique.problems;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import freemarker.template.TemplateExceptionHandler;
+
+/**
+ * Servlet implementation class ProblemView
+ */
+public class ProblemView extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	Configuration cfg;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ProblemView() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+    	String tampl_path = "/WEB-INF/templates";
+    	cfg = new Configuration(Configuration.VERSION_2_3_28);
+
+    	File fl= new File(getServletContext().getRealPath(tampl_path));
+    	try {
+			cfg.setDirectoryForTemplateLoading(fl);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    	
+    	cfg.setDefaultEncoding("UTF-8");
+    	cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+    	cfg.setLogTemplateExceptions(false);
+    	cfg.setWrapUncheckedExceptions(true);
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Template template = cfg.getTemplate("problems.ftl.html");
+		Writer out = response.getWriter();
+		Map<String, Object> data = new HashMap<String, Object>();
+		if(request.getSession().getAttribute("user")!=null) {
+			data.put("logged_in", 1);
+			data.put("username", request.getSession().getAttribute("user"));
+		}else {
+			data.put("logged_in", 0);
+		}
+		try {
+			template.process(data, out);
+		} catch (TemplateException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/templates/layouts/onlinejudge2019.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+	$('#problem_tables').DataTable();
+});
+// For demo to fit into DataTables site builder...
+$('#problem_tables').removeClass('display').addClass(
+		'table table-striped table-bordered');
